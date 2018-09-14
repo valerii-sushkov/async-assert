8433e9871dc09d8bf9cfb40f4d625280d0f9e9c8
valerii-sushkov <raziel01@bigmir.net>
2018-09-14 13:29:13 +0300

    Add tests and surefire pluging to run them.

---
+++ src/test/java/assertion/Tests.java
@@ -0,0 +1,55 @@
+package assertion;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Tests {
+
+    @Test
+    public void success() {
+        AsyncAssert.aAssert("Some description", () -> {
+            uglyDelay(5000);
+            return "response data";
+        }, data -> Assert.assertEquals(data, "response data"));
+
+        Assert.assertTrue(AsyncAssert.getAssertRecords().stream().anyMatch(r -> r.getTestId().contains("success")),
+                "No res in table after AAssert creation.");
+        Assert.assertTrue(AsyncAssert.getAssertRecords().stream()
+                        .filter(r -> r.getTestId().contains("success"))
+                        .map(r -> r.isSuccess())
+                        .findFirst().get() == false,
+                "Wait not complete but result already received!");
+        uglyDelay(5000);
+        Assert.assertTrue(AsyncAssert.getAssertRecords().stream()
+                        .filter(r -> r.getTestId().contains("success"))
+                        .map(r -> r.isSuccess())
+                        .findFirst().get() == true,
+                "Wait complete but result not successful");
+    }
+
+    @Test
+    public void fail() {
+        AsyncAssert.aAssert("Some description fail", () -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "response data bad";
+        }, data -> Assert.assertEquals(data, "response data"));
+        uglyDelay(5000);
+        Assert.assertTrue(AsyncAssert.getAssertRecords().stream()
+                        .filter(r -> r.getTestId().contains("success"))
+                        .map(r -> r.isSuccess())
+                        .findFirst().get() == true,
+                "Wait complete but result not successful");
+    }
+
+    private void uglyDelay(long delay) {
+        try {
+            Thread.sleep(delay);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
